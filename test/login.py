68dbee0cc9385d5d91fadd5954182f4577fdc0d5
经理 <manager@163.com>
2019-08-07 20:41:21 +0800

    分支操作

--- test/login.py
+++ test/login.py
@@ -2,9 +2,14 @@ number = '经理100'
 number = '张三 6666'
 
 number = '经理1222'
-<<<<<<< HEAD
+
 
 number = '经理999999'
-=======
+
 number = '张三88888'
->>>>>>> dfdc7c4a50fe3122dbc1d9b639eb4b2680eae7c2
+
+print('hello world!!!')
+
+
+
+
